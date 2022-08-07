4a232c4551ffe69366e7a8c9333c24a57a66dd37
enshi363 <enshi363@qq.com>
2022-08-07 22:03:48 +0800

    theme修改

--- lua/bootstrap.lua
+++ lua/bootstrap.lua
@@ -31,9 +31,10 @@ require('settings/whichkey')
 require('settings/hop')
 
 --require('nord').set()
-require('github-theme').setup({
-  theme_style = "dimmed",
-  dark_sidebar = true,
-  sidebars = {"qf", "vista_kind", "terminal", "packer"},
-  comment_style = "italic",
-})
+-- require('github-theme').setup({
+--   theme_style = "dimmed",
+--   dark_sidebar = true,
+--   sidebars = {"qf", "vista_kind", "terminal", "packer"},
+--   comment_style = "italic",
+-- })
+
