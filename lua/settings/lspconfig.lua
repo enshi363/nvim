c6e7d4e5f5695713b9c19c4701fd70824d48c8d8
shien <shi.en@opg.cn>
2022-10-18 09:22:24 +0800

    配置更新

--- lua/settings/lspconfig.lua
+++ lua/settings/lspconfig.lua
@@ -4,7 +4,8 @@ local util = require 'lspconfig/util'
 --local lsp_installer_servers = require('nvim-lsp-installer.servers')
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true 
 
 local servers = {
